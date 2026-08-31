745402fb792932080f5682854893f99532695985
dx1995 <33825118+dx1995@users.noreply.github.com>
2026-08-31 12:27:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
+# 修改PSG1218A分区表，从8MB扩展到16MB
+sed -i 's/reg = <0x50000 0x7b0000>/reg = <0x50000 0xfb0000>/' target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
