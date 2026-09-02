bee1dd54ffab17e5c71edb477caaa17028c13fc1
dx1995 <33825118+dx1995@users.noreply.github.com>
2026-09-02 08:55:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# 下载最新版 smartdns
-git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/smartdns.git package/smartdns
 
 # 下载最新版 passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 
 # 下载 passwall 依赖包
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
+
+# smartdns 使用 ImmortalWrt 自带 feeds 版本，不从 GitHub 拉最新版（最新版需要 Rust 编译器，21.02 不支持）
+
+# 删除需要 Rust 的包目录，避免编译报错
+rm -rf package/passwall_packages/shadowsocks-rust
+rm -rf package/passwall_packages/shadow-tls
